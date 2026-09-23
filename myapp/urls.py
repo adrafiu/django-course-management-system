1edd86142ee88fce02e88aca28e97403c279b4bb
Md. Abdul Rafeu <rafiu.diu@gmail.com>
2026-09-23 20:40:05 +0600

    fix path 2

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name= 'home'),
-    path('students/', views.student_list, name='student_list'), path('students/create/', views.student_create, name='student_create'), 
+    path('students/', views.student_list, name='student_list'), 
+    path('students/create/', views.student_create, name='student_create'), 
     path('students/update/<int:id>/', views.student_update, name='student_update'), 
     path('students/delete/<int:id>/', views.student_delete, name='student_delete'),
     path('results/', views.result_list, name='result_list'), 
